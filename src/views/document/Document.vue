d2c4892b227dd946c253d318dd3b1ce105ea3045
willerhe <willerhe@126.com>
2019-08-02 07:48:00 +0800

    fix bug

--- src/views/document/Document.vue
+++ src/views/document/Document.vue
@@ -3,10 +3,9 @@
         <el-row style="margin-bottom: 20px">
             <el-col :span="24">
                 <el-breadcrumb separator="/">
-                    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-                    <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
-                    <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-                    <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+                    <el-breadcrumb-item :to="{ path: '/' }">项目文档</el-breadcrumb-item>
+                    <el-breadcrumb-item><a href="/">小西门智能安防</a></el-breadcrumb-item>
+                    <el-breadcrumb-item>需求文档</el-breadcrumb-item>
                 </el-breadcrumb>
             </el-col>
         </el-row>
